5d76ff95d8036d0b01752ce0e889cda25b2b71bb
Huarachi Herrera Josue <huarachi.herrera.josue@gmail.com>
2021-08-15 10:47:32 -0400

    se creo el componente ejercicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EjerciciosComponent } from './components/ejercicios/ejercicios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EjerciciosComponent
   ],
   imports: [
     BrowserModule,
